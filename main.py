df3e73ea2763e3a2187deb19cdfa6a84fa0a07e1
steven2801 <ssteven075@gmail.com>
2023-11-05 22:25:45 +0700

    update

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ from urllib.request import Request
 from fastapi import FastAPI, Response, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import tensorflow as tf
-from utils import get_predicted_class
+from utils import get_predicted_class, transform_image
+from torchvision import models
+
 
 load_options = tf.saved_model.LoadOptions(
     experimental_io_device='/job:localhost')
 
 model = tf.saved_model.load("./model", options=load_options)
+
+
+# Make sure to set `weights` as `'IMAGENET1K_V1'` to use the pretrained weights:
+pt_model = models.densenet121(weights='IMAGENET1K_V1')
+# Since we are using our model only for inference, switch to `eval` mode:
+pt_model.eval()
 
 app = FastAPI()
 
@@ -43,6 +51,26 @@ def predict_image(uploaded_file: UploadFile, response: Response):
         predicted = get_predicted_class(img, model)
 
         return predicted
+    except Exception as e:
+        traceback.print_exc()
+        response.status_code = 500
+        return f"Internal Server Error: {e}"
+
+
+@app.post("/predict_image_2")
+def predict_image(uploaded_file: UploadFile, response: Response):
+    try:
+        if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
+            response.status_code = 400
+            return "File is Not an Image"
+
+        img = uploaded_file.file.read()
+        tensor = transform_image(image_bytes=img)
+
+        outputs = model.forward(tensor)
+        _, y_hat = outputs.max(1)
+        return y_hat
+
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import cv2
 import time
 import numpy as np
+import torchvision.transforms as transforms
+from PIL import Image
+import io
 
 
 def load_image_into_numpy_array(data):
@@ -10,6 +13,17 @@ def load_image_into_numpy_array(data):
         return img_np
     else:
         raise ValueError("Could not decode the image")
+
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
 
 
 def get_predicted_class(image, model):
